78b1a3ae8c20dd500dbb4891497a42e723de5e76
ramprasad@zelarsoft.com <ramprasad@zelarsoft.com>
2021-05-28 16:19:03 +0530

    commout to git

--- seed.groovy
+++ seed.groovy
@@ -1,36 +1,93 @@
-folder('CI-Pipelines') {
-    displayName('CI Pipelines')
-    description('CI Pipelines')
+folder('CI Pipelines'){
+    displayName ('CI Pipelines')
+    description ('CI Pipelines')
+}
+pipelineJob('CI Pipelines/frontend-ci'){
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/ramaprasadjenkins/frontend.git')
+
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
+pipelineJob('CI Pipelines/users-ci'){
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/ramaprasadjenkins/users.git')
+
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
 }
 
---//
-    pipelineJob("CI-Pipelines/${j}-ci") {
-        configure { flowdefinition ->
-            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-                'triggers' {
-                    'hudson.triggers.SCMTrigger' {
-                        'spec'('*/2 * * * 1-5')
-                        'ignorePostCommitHooks'(false)
+pipelineJob('CI Pipelines/login-ci'){
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/ramaprasadjenkins/login.git')
+
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
                     }
                 }
             }
-            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-                    'userRemoteConfigs' {
-                        'hudson.plugins.git.UserRemoteConfig' {
-                            'url'('https://github.com/ramaprasadjenkins/frontend.git')
-                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-                        }
-                    }
-                    'branches' {
-                        'hudson.plugins.git.BranchSpec' {
-                            'name'('*/main')
-                        }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
+
+
+
+
+pipelineJob('CI Pipelines/todo-ci'){
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/ramaprasadjenkins/todo.git')
+
                     }
                 }
-                'scriptPath'('Jenkinsfile')
-                'lightweight'(true)
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
             }
+            'scriptPath'('jenkinsfile')
+            'lightweight'(true)
         }
     }
 }
